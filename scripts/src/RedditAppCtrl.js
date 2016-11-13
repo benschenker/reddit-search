7193f96139ecdcc8f8ba43389ba4311417dd75ea
Benjamin Schenker <benjamin.schenker@ge.com>
2016-11-13 14:26:45 -0600

    Initial Commit

---
+++ scripts/src/RedditAppCtrl.js
@@ -0,0 +1,12 @@
+/* global angular*/
+/* eslint func-names: "off", no-param-reassign: "off"*/
+angular.module('redditApp')
+
+.controller('RedditAppCtrl',
+  [
+    '$scope',
+    ($scope) => {
+
+    },
+  ]
+);
