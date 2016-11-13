6515e17e29be3d9e070a356b8785548278166077
Benjamin Schenker <benjamin.schenker@ge.com>
2016-11-13 16:29:07 -0600

    add min ups filter

--- public/scripts/src/redditPosts.js
+++ public/scripts/src/redditPosts.js
@@ -3,9 +3,12 @@ angular.module('redditApp').component('redditPosts', {
   template: `
     <input type="number" ng-model="$ctrl.minComments" placeholder="Minimum # of Comments"/>
     <input type="number" ng-model="$ctrl.maxComments" placeholder="Maximum # of Comments"/>
+    <input type="number" ng-model="$ctrl.minUps" placeholder="Minimum # of Upvotes"/>
+
     <div ng-repeat="post in $ctrl.posts
           | filter: $ctrl.minCommentFilter
           | filter: $ctrl.maxCommentFilter
+          | filter: $ctrl.minUpsFilter
     ">
       <a href="{{post.url}}">{{post.title}}</a>
       <span>submitted by {{post.author}}</span>
@@ -15,6 +18,7 @@ angular.module('redditApp').component('redditPosts', {
     const ctrl = this;
     ctrl.minCommentFilter = (post) => post.num_comments > (ctrl.minComments || 0);
     ctrl.maxCommentFilter = (post) => post.num_comments < (ctrl.maxComments || Infinity);
+    ctrl.minUpsFilter = (post) => post.ups > (ctrl.minUps || 0);
   },
   bindings: {
     posts: '<',
