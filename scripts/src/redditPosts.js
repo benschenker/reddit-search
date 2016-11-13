d1129d82a39ad5a3591291dbaade082718d21b33
Benjamin Schenker <benjamin.schenker@ge.com>
2016-11-13 16:43:11 -0600

    add basic OR search filter

--- scripts/src/redditPosts.js
+++ scripts/src/redditPosts.js
@@ -4,11 +4,12 @@ angular.module('redditApp').component('redditPosts', {
     <input type="number" ng-model="$ctrl.minComments" placeholder="Minimum # of Comments"/>
     <input type="number" ng-model="$ctrl.maxComments" placeholder="Maximum # of Comments"/>
     <input type="number" ng-model="$ctrl.minUps" placeholder="Minimum # of Upvotes"/>
-
+    <input type="text" ng-model="$ctrl.textSearch" placeholder="Text search"/>
     <div ng-repeat="post in $ctrl.posts
           | filter: $ctrl.minCommentFilter
           | filter: $ctrl.maxCommentFilter
           | filter: $ctrl.minUpsFilter
+          | filter: $ctrl.textSearchORFilter
     ">
       <a href="{{post.url}}">{{post.title}}</a>
       <span>submitted by {{post.author}}</span>
@@ -19,6 +20,17 @@ angular.module('redditApp').component('redditPosts', {
     ctrl.minCommentFilter = (post) => post.num_comments > (ctrl.minComments || 0);
     ctrl.maxCommentFilter = (post) => post.num_comments < (ctrl.maxComments || Infinity);
     ctrl.minUpsFilter = (post) => post.ups > (ctrl.minUps || 0);
+
+    // Performs a text search on multiple tokens separated by a space and returns true if any pass
+    ctrl.textSearchORFilter = (post) => {
+      if (!ctrl.textSearch) {
+        return true;
+      }
+      const tokens = ctrl.textSearch.split(' ');
+      // TODO: make case insensitive
+      const matchingTokens = tokens.map((token) => post.title.indexOf(token) !== -1);
+      return matchingTokens.filter((isMatch) => isMatch).length > 0;
+    };
   },
   bindings: {
     posts: '<',
